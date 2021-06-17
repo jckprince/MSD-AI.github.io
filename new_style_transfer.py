346e59c332565d5a40e724e24ee959e152a1130d
amimoon <83853398+amimoon@users.noreply.github.com>
2021-06-17 12:44:39 +0900

    Add files via upload

---
+++ new_style_transfer.py
@@ -0,0 +1,278 @@
+import numpy as np
+import torch
+import torch.nn as nn
+#import torch.nn.functional as F
+from torch.autograd import Variable
+import tensorflow as tf
+import matplotlib.pyplot as plt        
+import os
+from PIL import Image
+
+
+# define Gram Matrix
+class GramMatrix(nn.Module):
+    def forward(self, y):
+        (b, ch, h, w) = y.size()
+        features = y.view(b, ch, w * h)
+        features_t = features.transpose(1, 2)
+        gram = features.bmm(features_t) / (ch * h * w)
+        return gram
+
+class Inspiration(nn.Module):
+    """ 
+    Inspiration Layer (from MSG-Net paper)
+    tuning the featuremap with target Gram Matrix
+    ref https://arxiv.org/abs/1703.06953
+    """
+    def __init__(self, C, B=1):
+        super(Inspiration, self).__init__()
+        # B is equal to 1 or input mini_batch
+        self.weight = nn.Parameter(torch.Tensor(1,C,C), requires_grad=True)
+        # non-parameter buffer
+        self.G = Variable(torch.Tensor(B,C,C), requires_grad=True)
+        self.C = C
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.weight.data.uniform_(0.0, 0.02)
+
+    def setTarget(self, target):
+        self.G = target
+
+    def forward(self, X):
+        # input X is a 3D feature map
+        self.P = torch.bmm(self.weight.expand_as(self.G),self.G)
+        return torch.bmm(self.P.transpose(1,2).expand(X.size(0), self.C, self.C), X.view(X.size(0),X.size(1),-1)).view_as(X)
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(' \
+            + 'N x ' + str(self.C) + ')'
+
+class ConvLayer(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride):
+        super(ConvLayer, self).__init__()
+        reflection_padding = int(np.floor(kernel_size / 2))
+        self.reflection_pad = nn.ReflectionPad2d(reflection_padding)
+        self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+
+    def forward(self, x):
+        out = self.reflection_pad(x)
+        out = self.conv2d(out)
+        return out
+
+class UpsampleConvLayer(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, upsample=None):
+        super(UpsampleConvLayer, self).__init__()
+        self.upsample = upsample
+        if upsample:
+            self.upsample_layer = torch.nn.Upsample(scale_factor=upsample)
+        self.reflection_padding = int(np.floor(kernel_size / 2))
+        if self.reflection_padding != 0:
+            self.reflection_pad = nn.ReflectionPad2d(self.reflection_padding)
+        self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+
+    def forward(self, x):
+        if self.upsample:
+            x = self.upsample_layer(x)
+        if self.reflection_padding != 0:
+            x = self.reflection_pad(x)
+        out = self.conv2d(x)
+        return out
+
+class Bottleneck(nn.Module):
+    """ Pre-activation residual block
+    Identity Mapping in Deep Residual Networks
+    ref https://arxiv.org/abs/1603.05027
+    """
+    def __init__(self, inplanes, planes, stride=1, downsample=None, norm_layer=nn.BatchNorm2d):
+        super(Bottleneck, self).__init__()
+        self.expansion = 4
+        self.downsample = downsample
+        if self.downsample is not None:
+            self.residual_layer = nn.Conv2d(inplanes, planes * self.expansion,
+                                                        kernel_size=1, stride=stride)
+        conv_block = []
+        conv_block += [norm_layer(inplanes, track_running_stats=True),
+                                    nn.ReLU(inplace=True),
+                                    nn.Conv2d(inplanes, planes, kernel_size=1, stride=1)]
+        conv_block += [norm_layer(planes, track_running_stats=True),
+                                    nn.ReLU(inplace=True),
+                                    ConvLayer(planes, planes, kernel_size=3, stride=stride)]
+        conv_block += [norm_layer(planes, track_running_stats=True),
+                                    nn.ReLU(inplace=True),
+                                    nn.Conv2d(planes, planes * self.expansion, kernel_size=1, stride=1)]
+        self.conv_block = nn.Sequential(*conv_block)
+        
+    def forward(self, x):
+        if self.downsample is not None:
+            residual = self.residual_layer(x)
+        else:
+            residual = x
+        return residual + self.conv_block(x)
+    
+class UpBottleneck(nn.Module):
+    """ Up-sample residual block (from MSG-Net paper)
+    Enables passing identity all the way through the generator
+    ref https://arxiv.org/abs/1703.06953
+    """
+    def __init__(self, inplanes, planes, stride=2, norm_layer=nn.BatchNorm2d):
+        super(UpBottleneck, self).__init__()
+        self.expansion = 4
+        self.residual_layer = UpsampleConvLayer(inplanes, planes * self.expansion,
+                                                      kernel_size=1, stride=1, upsample=stride)
+        conv_block = []
+        conv_block += [norm_layer(inplanes, track_running_stats=True),
+                                    nn.ReLU(inplace=True),
+                                    nn.Conv2d(inplanes, planes, kernel_size=1, stride=1)]
+        conv_block += [norm_layer(planes, track_running_stats=True),
+                                    nn.ReLU(inplace=True),
+                                    UpsampleConvLayer(planes, planes, kernel_size=3, stride=1, upsample=stride)]
+        conv_block += [norm_layer(planes, track_running_stats=True),
+                                    nn.ReLU(inplace=True),
+                                    nn.Conv2d(planes, planes * self.expansion, kernel_size=1, stride=1)]
+        self.conv_block = nn.Sequential(*conv_block)
+
+    def forward(self, x):
+        return  self.residual_layer(x) + self.conv_block(x)
+
+class Net(nn.Module):
+    """The MSG-Net."""
+    def __init__(self, input_nc=3, output_nc=3, ngf=64, norm_layer=nn.InstanceNorm2d, n_blocks=6, gpu_ids=[]):
+        super(Net, self).__init__()
+        self.gpu_ids = gpu_ids
+        self.gram = GramMatrix()
+
+        block = Bottleneck
+        upblock = UpBottleneck
+        expansion = 4
+
+        model1 = []
+        model1 += [ConvLayer(input_nc, 64, kernel_size=7, stride=1),
+                            norm_layer(64, track_running_stats=True),
+                            nn.ReLU(inplace=True),  
+                            block(64, 32, 2, 1, norm_layer),
+                            block(32*expansion, ngf, 2, 1, norm_layer)]
+        self.model1 = nn.Sequential(*model1)
+
+        model = []
+        self.ins = Inspiration(ngf*expansion)
+        model += [self.model1]
+        model += [self.ins]    
+
+        for i in range(n_blocks):
+            model += [block(ngf*expansion, ngf, 1, None, norm_layer)]
+        
+        model += [upblock(ngf*expansion, 32, 2, norm_layer),
+                            upblock(32*expansion, 16, 2, norm_layer),
+                            norm_layer(16*expansion, track_running_stats=True),
+                            nn.ReLU(inplace=True),
+                            ConvLayer(16*expansion, output_nc, kernel_size=7, stride=1)]
+
+        self.model = nn.Sequential(*model)
+
+    def setTarget(self, Xs):
+        F = self.model1(Xs)
+        G = self.gram(F)
+        self.ins.setTarget(G)
+
+    def forward(self, input):
+        return self.model(input)
+
+def tensor_load_rgbimage(filename, size=None, scale=None, keep_asp=False):
+    img = Image.open(filename).convert('RGB')
+    if size is not None:
+        if keep_asp:
+            size2 = int(size * 1.0 / img.size[0] * img.size[1])
+            img = img.resize((size, size2), Image.ANTIALIAS)
+        else:
+            img = img.resize((size, size), Image.ANTIALIAS)
+
+    elif scale is not None:
+        img = img.resize((int(img.size[0] / scale), int(img.size[1] / scale)), Image.ANTIALIAS)
+    img = np.array(img).transpose(2, 0, 1)
+    img = torch.from_numpy(img).float()
+    return img
+
+def tensor_save_rgbimage(tensor, filename, cuda=False):
+    if cuda:
+        img = tensor.clone().cpu().clamp(0, 255).numpy()
+    else:
+        img = tensor.clone().clamp(0, 255).numpy()
+    img = img.transpose(1, 2, 0).astype('uint8')
+    img = Image.fromarray(img)
+    img.save(filename)
+
+
+def tensor_save_bgrimage(tensor, filename, cuda=False):
+    (b, g, r) = torch.chunk(tensor, 3)
+    tensor = torch.cat((r, g, b))
+    tensor_save_rgbimage(tensor, filename, cuda)
+    
+def preprocess_batch(batch):
+    batch = batch.transpose(0, 1)
+    (r, g, b) = torch.chunk(batch, 3)
+    batch = torch.cat((b, g, r))
+    batch = batch.transpose(0, 1)
+    return batch
+
+def load_img(path_to_img):
+    max_dim = 512
+    img = tf.io.read_file(path_to_img)
+    img = tf.image.decode_image(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    long_dim = max(shape)
+    scale = max_dim / long_dim
+
+    new_shape = tf.cast(shape * scale, tf.int32)
+
+    img = tf.image.resize(img, new_shape)
+    img = img[tf.newaxis, :]
+    return img
+
+def imshow(image, title=None):
+    if len(image.shape) > 3:
+        image = tf.squeeze(image, axis=0)
+
+    plt.imshow(image)
+    
+    if title:
+        plt.title(title)
+
+#import wget
+def main(refer_img_path, target_img_path):
+    # image 파일명
+    refer_img = refer_img_path.split('/')[-1]
+    target_img = target_img_path.split('/')[-1]
+    
+    style_reference_image_path = './static/images/nst_get/'+ refer_img	# 스타일 참조 이미지 
+    target_image_path = './static/images/nst_get/user/'+ target_img # 타깃 이미지 
+    
+    content_path = target_image_path 
+    style_path = style_reference_image_path
+    
+    content_image = tensor_load_rgbimage(content_path, size=512, keep_asp=True).unsqueeze(0)
+    style = tensor_load_rgbimage(style_path, size=512).unsqueeze(0)    
+    style = preprocess_batch(style)
+
+    #wget.download('https://www.dropbox.com/s/2iz8orqqubrfrpo/21styles.model?dl=1', '21styles.model')
+    os.system('wget -O 21styles.model https://www.dropbox.com/s/2iz8orqqubrfrpo/21styles.model?dl=1')
+    style_model = Net(ngf=128)
+    style_model.load_state_dict(torch.load('./21styles.model'),False)
+    
+    style_v = Variable(style)
+    content_image = Variable(preprocess_batch(content_image))
+    style_model.setTarget(style_v)
+    output = style_model(content_image)
+    
+    refer_img_name = refer_img_path.split('.')[0].split('/')[-1]
+    target_img_name = target_img_path.split('.')[0].split('/')[-1]
+    fname = './static/images/nst_result/'+target_img_name+'('+refer_img_name +').JPG'
+    tensor_save_bgrimage(output.data[0], fname, False)
+    #save_img(fname, img)
+
+    return fname
+    
+if __name__ == "__main__":
+	main()
